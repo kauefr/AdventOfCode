8bc413879095afb76ec9aaa6fb0f022d301c90ac
Kauê Rodrigues <kauefr@gmail.com>
2024-08-04 19:34:34 -0300

    split into 2 functions

--- src/main.c
+++ src/main.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int main(int argc, char **argv) {
+int direct_mode(int argc, char **argv) {
     int return_value = EXIT_SUCCESS;
     if (argc != 2) {
         fprintf(stderr, "Error: 1 argument expected, %d received\n", argc-1);
         return_value = EXIT_FAILURE;
         goto RETURN;
-
     }
 
     FILE *fp;
@@ -18,7 +17,6 @@ int main(int argc, char **argv) {
         goto RETURN;
     }
     //fp is open
-
 
     int sum = 0;
     char buffer[BUFSIZ];
@@ -47,11 +45,25 @@ int main(int argc, char **argv) {
         goto CLOSE;
     }
 
-
-
     printf("Result: %d\n", sum);
+
     CLOSE:
     fclose(fp);
+    //fp is closed
     RETURN:
     return return_value;
+}
+
+int interactive_mode() {
+    fprintf(stderr, "Error: Interactive mode not implemented.\n");
+    return EXIT_FAILURE;
+}
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        return interactive_mode();
+    }
+    else {
+        return direct_mode(argc, argv);
+    }
 }
